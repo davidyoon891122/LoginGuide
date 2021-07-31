995485d61506d5ee586e9fcb99a776bf1f23d2de
davidyoon <qqpo12@naver.com>
2021-07-31 18:41:33 +0900

    add NavigationControlView for Home View, set AppDelegate point to MainNavigationController

--- LoginGuide/Controller/MainNavigationController.swift
+++ LoginGuide/Controller/MainNavigationController.swift
@@ -11,6 +11,37 @@ class MainNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        print("MainNavigationController")
+        
+        if isLoggedIn() {
+            let homeController = HomeController()
+            viewControllers = [homeController]
+        } else {
+            perform(#selector(showLoginController), with: nil, afterDelay: 0.01)
+        }
+        
+        
+    }
+    
+    fileprivate func isLoggedIn() -> Bool {
+        return true
+    }
+    
+    
+    @objc func showLoginController() {
+        let loginController = LoginController()
+        loginController.modalPresentationStyle = .fullScreen
+        present(loginController, animated: true, completion: {
+            //
+        })
+    }
+}
+
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        navigationItem.title = "We're logged in"
     }
 }
