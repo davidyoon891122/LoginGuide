3eb4ee6b34bbc656c79883cf28ad56ed541deb78
davidyoon <qqpo12@naver.com>
2021-07-31 18:20:06 +0900

    remove SceneDelegate, remove sceneDelegate settings on info.plist

--- LoginGuide/Controller/LoginController.swift
+++ LoginGuide/Controller/LoginController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+class LoginController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
 
     
     // create collection view with closure
---
+++ LoginGuide/Controller/MainNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  MainNavigationController.swift
+//  LoginGuide
+//
+//  Created by David Yoon on 2021/07/31.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        print("MainNavigationController")
+    }
+}
